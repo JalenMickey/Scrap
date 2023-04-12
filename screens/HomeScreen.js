b296a2d93eb7be1e8c647676b28e83f3b2c438cd
Jalen Mickey <jalen@Jalens-MacBook-Pro.local>
2023-04-12 06:56:58 -0500

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView} from 'react-native'
+import React from 'react'
+import header from '../components/home/header'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <header/>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
